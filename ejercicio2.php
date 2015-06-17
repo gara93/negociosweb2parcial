0d27ffa63d6e098b84cfbadff6788e695047ce92
Estudiante <gara9315@gmail.com>
2015-06-16 20:01:13 -0600

    Actualizando tercer  hecho en clase

---
+++ ejercicio2.php
@@ -0,0 +1,23 @@
+<?php
+  $host = "127.0.0.1";
+  $user = "root";
+  $pswd = "root";
+  $db = "nw201502";
+
+  $conn = new mysqli($host, $user, $pswd, $db);
+
+  if($conn->errno){
+    die("ERROR". $conn->error);
+
+  }
+
+  $productos = $conn->query("select * from productos;");
+  $prdArr = array();
+  while ($producto = productos->fetch_assoc()) {
+    # code...
+    $prdArr[] = $producto;
+  }
+
+  print_r($prdArr);
+
+?>
